77244c21cd12c1563943d5b2c69e294147d67813
Chaitanya Kukwas <chaiku1113@gmail.com>
2026-04-14 22:19:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort the Half Sorted/sort-the-half-sorted.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+import java.util.Arrays;
+
+class Solution {
+    public static void sortHalves(int arr[], int n) {
+        // your code here
+        Arrays.sort(arr);
+    }
+}
