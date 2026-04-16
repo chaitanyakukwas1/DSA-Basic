7c77725d08a0cf108a4fc0e43f34976545d24bb1
Chaitanya Kukwas <chaiku1113@gmail.com>
2026-04-16 23:14:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+
+class Solution {
+    int arraySum(int arr[]) {
+        // code here
+        int i=0;
+        int a=0;
+        while(i<arr.length)
+        {
+            a = a+arr[i];
+            i++;
+        }
+        return a;
+    }
+}
