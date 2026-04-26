05940ccfc7cb9a37877d1786739c14ab4282366d
Chaitanya Kukwas <chaiku1113@gmail.com>
2026-04-26 21:16:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print Linked List/print-linked-list.java
+++ Difficulty: Basic/Print Linked List/print-linked-list.java
@@ -1,14 +1,25 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}*/
 
 class Solution {
     public ArrayList<Integer> printList(Node head) {
         // code here
-        ArrayList<Integer>List = new ArrayList<>();
-       Node temp = head;
-       while(temp != null)
-       {
-           List.add(temp.data);
-           temp = temp.next;
-       }
-       return List;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        Node curr = head;
+        
+        while(curr != null)
+        {
+            list.add(curr.data);
+            curr = curr.next;
+        }
+        return list;
     }
 }
