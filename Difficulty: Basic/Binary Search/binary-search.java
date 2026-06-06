1ede4bdbd44a0ec7a58c0dd522c8c368dbc2476b
Chaitanya Kukwas <chaiku1113@gmail.com>
2026-06-06 22:07:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Binary Search/binary-search.java
+++ Difficulty: Basic/Binary Search/binary-search.java
@@ -1,20 +1,22 @@
 class Solution {
-    public boolean binarySearch(List<Integer> arr, int k) {
-        // code here
+    public boolean binarySearch(int[] arr, int k) {
         int low = 0;
-        int high = arr.size() - 1;
+        int high = arr.length - 1;
+        
         while(low<=high)
         {
             int mid = low + (high - low)/2;
-            if(arr.get(mid) == k)
+            if(arr[mid] == k)
             {
                 return true;
             }
-            if(arr.get(mid)< k)
+            
+            if(arr[mid] < k)
             {
-                low = mid+1;
+                low = mid + 1;
             }
-            else
+            
+            if(arr[mid] > k)
             {
                 high = mid - 1;
             }
